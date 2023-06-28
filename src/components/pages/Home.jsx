e9cdff42f0172e194af4daf3e92888bdaacfc2af
alanbirru <alanb_99hotmail.com>
2023-06-28 12:16:11 -0700

    puting hero info

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,14 +1,27 @@
 const Home = () => {
   return (
     <section
-      className={`h-[800px] bg-[rgba(0,0,0,0.482)] bg-[url("hero.jpg")] bg-cover  bg-center bg-no-repeat pt-3  bg-blend-darken md:h-[1000px]`}
+      className={`h-[750px] bg-[rgba(0,0,0,0.54)] bg-[url("hero.jpg")] bg-cover  bg-center bg-no-repeat pt-3  bg-blend-darken md:h-[1000px]`}
       id="Home"
     >
-      <div className="flex flex-col">
-        <h1 className="text-white">Lorem ipsum dolor sit amet.</h1>
-        <h2 className="text-white">Lorem ipsum dolor sit amet.</h2>
-        <h3 className="text-white">Lorem ipsum dolor sit amet.</h3>
-        <h4 className="text-white">Lorem ipsum dolor sit amet.</h4>
+      <div className=" flex flex-col items-center justify-center gap-2    pt-60">
+        <div className="  rounded-md bg-[#ffffff2d] p-5 backdrop-blur-md">
+          <h1 className="  mb-4 text-center text-5xl text-white md:w-[600px] md:text-8xl">
+            <span className="text-[orange]">FIMAZ</span> 2023
+          </h1>
+          <h4 className="text-center  text-white">
+            Feria Inmobiliaria de mazatlan
+          </h4>
+        </div>
+
+        <div className="container mx-auto text-center">
+          <p className="text-md pt-5 text-white md:text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+            labore nesciunt ea, nemo incidunt ullam architecto quod atque amet
+            mollitia, quia rem consequatur odio esse culpa velit magni quos
+            doloribus numquam voluptatum provident quis.
+          </p>
+        </div>
       </div>
     </section>
   );
