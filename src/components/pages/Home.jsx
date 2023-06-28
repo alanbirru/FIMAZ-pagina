afc1ef51430ad97dfa651755c524794722bd0c22
alanbirru <alanb_99hotmail.com>
2023-06-28 10:26:55 -0700

    configuring headings size and fonts

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,7 +4,12 @@ const Home = () => {
       className={`h-[800px] bg-[rgba(0,0,0,0.482)] bg-[url("hero.jpg")] bg-cover  bg-center bg-no-repeat pt-3  bg-blend-darken md:h-[1000px]`}
       id="Home"
     >
-      <div className="flex items-center justify-between"></div>
+      <div className="flex flex-col">
+        <h1 className="text-white">Lorem ipsum dolor sit amet.</h1>
+        <h2 className="text-white">Lorem ipsum dolor sit amet.</h2>
+        <h3 className="text-white">Lorem ipsum dolor sit amet.</h3>
+        <h4 className="text-white">Lorem ipsum dolor sit amet.</h4>
+      </div>
     </section>
   );
 };
