cdd55847aa6f449b9b5538790b34b450188dd55b
alanbirru <alanb_99hotmail.com>
2023-06-28 15:14:19 -0700

    ajusting hero elements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
 const Home = () => {
   return (
     <section
-      className={`h-[700px]  border-b-8 border-[orange] bg-[rgba(0,0,0,0.6)]  bg-[url("hero.jpg")] bg-cover bg-center  bg-no-repeat pt-3 bg-blend-darken md:h-[800px]`}
+      className={`h-[650px]  border-b-8 border-[orange] bg-[rgba(0,0,0,0.6)]  bg-[url("hero.jpg")] bg-cover bg-center  bg-no-repeat pt-3 bg-blend-darken md:h-[700px]`}
       id="Home"
     >
-      <div className=" flex flex-col items-center justify-center gap-2 pt-40">
+      <div className=" flex flex-col items-center justify-center gap-2 pt-24">
         <div className="  rounded-md bg-[#ffffff2d] p-5 backdrop-blur-md">
           <h1 className="  mb-4 text-center text-6xl text-white md:w-[600px] md:text-8xl">
             <span className="text-[orange]">FIMAZ</span> 2023
