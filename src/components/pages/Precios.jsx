ca487d841afd116cf614bec69d5b16a057a4469c
alanbirru <alanb_99hotmail.com>
2023-07-11 16:56:25 -0700

    delete animations

--- src/components/pages/Precios.jsx
+++ src/components/pages/Precios.jsx
@@ -1,141 +1,83 @@
-import { Fade } from "react-awesome-reveal";
 const Precios = ({ paddingTop, title, id }) => {
   return (
-    <section className={`${paddingTop}`} id={id}>
-      <Fade triggerOnce delay={3000}>
-        <>
-          {title}
-          <div>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Consequuntur, possimus tenetur assumenda et veniam eligendi adipisci
-            ad. Magni consequuntur odio voluptates, cupiditate unde fuga aperiam
-            nobis. Dicta veritatis excepturi officiis aut, harum, odit sapiente
-            suscipit autem iste magnam atque libero. Ipsa harum itaque maiores.
-            Debitis corrupti repudiandae nesciunt illo perspiciatis. Asperiores
-            minima quis, est ipsum assumenda eum animi facere, sit non quae
-            illum blanditiis labore dolorem ducimus! Rerum, accusantium quos
-            corrupti dolores sed possimus obcaecati et iure consequuntur
-            doloremque aspernatur perspiciatis non voluptate aliquid ad neque
-            fugiat modi? Repellat atque consectetur numquam fugiat repellendus
-            accusantium, cupiditate dolorem aperiam consequuntur quisquam
-            aliquam sit explicabo ipsum eum obcaecati nisi cumque? Sequi
-            consequatur aut enim aliquam quis blanditiis vitae rem Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Consequuntur, possimus
-            tenetur assumenda et veniam eligendi adipisci ad. Magni consequuntur
-            odio voluptates, cupiditate unde fuga aperiam nobis. Dicta veritatis
-            excepturi officiis aut, harum, odit sapiente suscipit autem iste
-            magnam atque libero. Ipsa harum itaque maiores. Debitis corrupti
-            repudiandae nesciunt illo perspiciatis. Asperiores minima quis, est
-            ipsum assumenda eum animi facere, sit non quae illum blanditiis
-            labore dolorem ducimus! Rerum, accusantium quos corrupti dolores sed
-            possimus obcaecati et iure consequuntur doloremque aspernatur
-            perspiciatis non voluptate aliquid ad neque fugiat modi? Repellat
-            atque consectetur numquam fugiat repellendus accusantium, cupiditate
-            dolorem aperiam consequuntur quisquam aliquam sit explicabo ipsum
-            eum obcaecati nisi cumque? Sequi consequatur aut enim aliquam quis
-            blanditiis vitae rem Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. Consequuntur, possimus tenetur assumenda et veniam
-            eligendi adipisci ad. Magni consequuntur odio voluptates, cupiditate
-            unde fuga aperiam nobis. Dicta veritatis excepturi officiis aut,
-            harum, odit sapiente suscipit autem iste magnam atque libero. Ipsa
-            harum itaque maiores. Debitis corrupti repudiandae nesciunt illo
-            perspiciatis. Asperiores minima quis, est ipsum assumenda eum animi
-            facere, sit non quae illum blanditiis labore dolorem ducimus! Rerum,
-            accusantium quos corrupti dolores sed possimus obcaecati et iure
-            consequuntur doloremque aspernatur perspiciatis non voluptate
-            aliquid ad neque fugiat modi? Repellat atque consectetur numquam
-            fugiat repellendus accusantium, cupiditate dolorem aperiam
-            consequuntur quisquam aliquam sit explicabo ipsum eum obcaecati nisi
-            cumque? Sequi consequatur aut enim aliquam quis blanditiis vitae rem
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Consequuntur, possimus tenetur assumenda et veniam eligendi adipisci
-            ad. Magni consequuntur odio voluptates, cupiditate unde fuga aperiam
-            nobis. Dicta veritatis excepturi officiis aut, harum, odit sapiente
-            suscipit autem iste magnam atque libero. Ipsa harum itaque maiores.
-            Debitis corrupti repudiandae nesciunt illo perspiciatis. Asperiores
-            minima quis, est ipsum assumenda eum animi facere, sit non quae
-            illum blanditiis labore dolorem ducimus! Rerum, accusantium quos
-            corrupti dolores sed possimus obcaecati et iure consequuntur
-            doloremque aspernatur perspiciatis non voluptate aliquid ad neque
-            fugiat modi? Repellat atque consectetur numquam fugiat repellendus
-            accusantium, cupiditate dolorem aperiam consequuntur quisquam
-            aliquam sit explicabo ipsum eum obcaecati nisi cumque? Sequi
-            consequatur aut enim aliquam quis blanditiis vitae rem Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Consequuntur, possimus
-            tenetur assumenda et veniam eligendi adipisci ad. Magni consequuntur
-            odio voluptates, cupiditate unde fuga aperiam nobis. Dicta veritatis
-            excepturi officiis aut, harum, odit sapiente suscipit autem iste
-            magnam atque libero. Ipsa harum itaque maiores. Debitis corrupti
-            repudiandae nesciunt illo perspiciatis. Asperiores minima quis, est
-            ipsum assumenda eum animi facere, sit non quae illum blanditiis
-            labore dolorem ducimus! Rerum, accusantium quos corrupti dolores sed
-            possimus obcaecati et iure consequuntur doloremque aspernatur
-            perspiciatis non voluptate aliquid ad neque fugiat modi? Repellat
-            atque consectetur numquam fugiat repellendus accusantium, cupiditate
-            dolorem aperiam consequuntur quisquam aliquam sit explicabo ipsum
-            eum obcaecati nisi cumque? Sequi consequatur aut enim aliquam quis
-            blanditiis vitae rem Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. Consequuntur, possimus tenetur assumenda et veniam
-            eligendi adipisci ad. Magni consequuntur odio voluptates, cupiditate
-            unde fuga aperiam nobis. Dicta veritatis excepturi officiis aut,
-            harum, odit sapiente suscipit autem iste magnam atque libero. Ipsa
-            harum itaque maiores. Debitis corrupti repudiandae nesciunt illo
-            perspiciatis. Asperiores minima quis, est ipsum assumenda eum animi
-            facere, sit non quae illum blanditiis labore dolorem ducimus! Rerum,
-            accusantium quos corrupti dolores sed possimus obcaecati et iure
-            consequuntur doloremque aspernatur perspiciatis non voluptate
-            aliquid ad neque fugiat modi? Repellat atque consectetur numquam
-            fugiat repellendus accusantium, cupiditate dolorem aperiam
-            consequuntur quisquam aliquam sit explicabo ipsum eum obcaecati nisi
-            cumque? Sequi consequatur aut enim aliquam quis blanditiis vitae rem
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Consequuntur, possimus tenetur assumenda et veniam eligendi adipisci
-            ad. Magni consequuntur odio voluptates, cupiditate unde fuga aperiam
-            nobis. Dicta veritatis excepturi officiis aut, harum, odit sapiente
-            suscipit autem iste magnam atque libero. Ipsa harum itaque maiores.
-            Debitis corrupti repudiandae nesciunt illo perspiciatis. Asperiores
-            minima quis, est ipsum assumenda eum animi facere, sit non quae
-            illum blanditiis labore dolorem ducimus! Rerum, accusantium quos
-            corrupti dolores sed possimus obcaecati et iure consequuntur
-            doloremque aspernatur perspiciatis non voluptate aliquid ad neque
-            fugiat modi? Repellat atque consectetur numquam fugiat repellendus
-            accusantium, cupiditate dolorem aperiam consequuntur quisquam
-            aliquam sit explicabo ipsum eum obcaecati nisi cumque? Sequi
-            consequatur aut enim aliquam quis blanditiis vitae rem Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Consequuntur, possimus
-            tenetur assumenda et veniam eligendi adipisci ad. Magni consequuntur
-            odio voluptates, cupiditate unde fuga aperiam nobis. Dicta veritatis
-            excepturi officiis aut, harum, odit sapiente suscipit autem iste
-            magnam atque libero. Ipsa harum itaque maiores. Debitis corrupti
-            repudiandae nesciunt illo perspiciatis. Asperiores minima quis, est
-            ipsum assumenda eum animi facere, sit non quae illum blanditiis
-            labore dolorem ducimus! Rerum, accusantium quos corrupti dolores sed
-            possimus obcaecati et iure consequuntur doloremque aspernatur
-            perspiciatis non voluptate aliquid ad neque fugiat modi? Repellat
-            atque consectetur numquam fugiat repellendus accusantium, cupiditate
-            dolorem aperiam consequuntur quisquam aliquam sit explicabo ipsum
-            eum obcaecati nisi cumque? Sequi consequatur aut enim aliquam quis
-            blanditiis vitae rem numquam fugiat repellendus accusantium,
-            cupiditate dolorem aperiam consequuntur quisquam aliquam sit
-            explicabo ipsum eum obcaecati nisi cumque? Sequi consequatur aut
-            enim aliquam quis blanditiis vitae rem Lorem ipsum dolor sit, amet
-            consectetur adipisicing elit. Consequuntur, possimus tenetur
-            assumenda et veniam eligendi adipisci ad. Magni consequuntur odio
-            voluptates, cupiditate unde fuga aperiam nobis. Dicta veritatis
-            excepturi officiis aut, harum, odit sapiente suscipit autem iste
-            magnam atque libero. Ipsa harum itaque maiores. Debitis corrupti
-            repudiandae nesciunt illo perspiciatis. Asperiores minima quis, est
-            ipsum assumenda eum animi facere, sit non quae illum blanditiis
-            labore dolorem ducimus! Rerum, accusantium quos corrupti dolores sed
-            possimus obcaecati et iure consequuntur doloremque aspernatur
-            perspiciatis non voluptate aliquid ad neque fugiat modi? Repellat
-            atque consectetur numquam fugiat repellendus accusantium, cupiditate
-            dolorem aperiam consequuntur quisquam aliquam sit explicabo ipsum
-            eum obcaecati nisi cumque? Sequi consequatur aut enim aliquam quis
-            blanditiis vitae rem
-          </div>
-        </>
-      </Fade>
+    <section className={`${paddingTop} `} id={id}>
+      <>
+        {title}
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Consequuntur, possimus tenetur assumenda et veniam eligendi adipisci
+          ad. Magni consequuntur odio voluptates, cupiditate unde fuga aperiam
+          nobis. Dicta veritatis excepturi officiis aut, harum, odit sapiente
+          suscipit autem iste magnam atque libero. Ipsa harum itaque maiores.
+          Debitis corrupti repudiandae nesciunt illo perspiciatis. Asperiores
+          minima quis, est ipsum assumenda eum animi facere, sit non quae illum
+          blanditiis labore dolorem ducimus! Rerum, accusantium quos corrupti
+          dolores sed possimus obcaecati et iure consequuntur doloremque
+          aspernatur perspiciatis non voluptate aliquid ad neque fugiat modi?
+          Repellat atque consectetur numquam fugiat repellendus accusantium,
+          cupiditate dolorem aperiam consequuntur quisquam aliquam sit explicabo
+          ipsum eum obcaecati nisi cumque? Sequi consequatur aut enim aliquam
+          quis blanditiis vitae rem Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Consequuntur, possimus tenetur assumenda et veniam
+          eligendi adipisci ad. Magni consequuntur odio voluptates, cupiditate
+          unde fuga aperiam nobis. Dicta veritatis excepturi officiis aut,
+          harum, odit sapiente suscipit autem iste magnam atque libero. Ipsa
+          harum itaque maiores. Debitis corrupti repudiandae nesciunt illo
+          perspiciatis. Asperiores minima quis, est ipsum assumenda eum animi
+          facere, sit non quae illum blanditiis labore dolorem ducimus! Rerum,
+          accusantium quos corrupti dolores sed possimus obcaecati et iure
+          consequuntur doloremque aspernatur perspiciatis non voluptate aliquid
+          ad neque fugiat modi? Repellat atque consectetur numquam fugiat
+          repellendus accusantium, cupiditate dolorem aperiam consequuntur
+          quisquam aliquam sit explicabo ipsum eum obcaecati nisi cumque? Sequi
+          consequatur aut enim aliquam quis blanditiis vitae rem Lorem ipsum
+          dolor sit, amet consectetur adipisicing elit. Consequuntur, possimus
+          tenetur assumenda et veniam eligendi adipisci ad. Magni consequuntur
+          odio voluptates, cupiditate unde fuga aperiam nobis. Dicta veritatis
+          excepturi officiis aut, harum, odit sapiente suscipit autem iste
+          magnam atque libero. Ipsa harum itaque maiores. Debitis corrupti
+          repudiandae nesciunt illo perspiciatis. Asperiores minima quis, est
+          ipsum assumenda eum animi facere, sit non quae illum blanditiis labore
+          dolorem ducimus! Rerum, accusantium quos corrupti dolores sed possimus
+          obcaecati et iure consequuntur doloremque aspernatur perspiciatis non
+          voluptate aliquid ad neque fugiat modi? Repellat atque consectetur
+          numquam fugiat repellendus accusantium, cupiditate dolorem aperiam
+          consequuntur quisquam aliquam sit explicabo ipsum eum obcaecati nisi
+          cumque? Sequi consequatur aut enim aliquam quis blanditiis vitae rem
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Consequuntur, possimus tenetur assumenda et veniam eligendi adipisci
+          ad. Magni consequuntur odio voluptates, cupiditate unde fuga aperiam
+          nobis. Dicta veritatis excepturi officiis aut, harum, odit sapiente
+          suscipit autem iste magnam atque libero. Ipsa harum itaque maiores.
+          Debitis corrupti repudiandae nesciunt illo perspiciatis. Asperiores
+          minima quis, est ipsum assumenda eum animi facere, sit non quae illum
+          blanditiis labore dolorem ducimus! Rerum, accusantium quos corrupti
+          dolores sed possimus obcaecati et iure consequuntur doloremque
+          aspernatur perspiciatis non voluptate aliquid ad neque fugiat modi?
+          Repellat atque consectetur numquam fugiat repellendus accusantium,
+          cupiditate dolorem aperiam consequuntur quisquam aliquam sit explicabo
+          ipsum eum obcaecati nisi cumque? Sequi consequatur aut enim aliquam
+          quis blanditiis vitae rem Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Consequuntur, possimus tenetur assumenda et veniam
+          eligendi adipisci ad. Magni consequuntur odio voluptates, cupiditate
+          unde fuga aperiam nobis. Dicta veritatis excepturi officiis aut,
+          harum, odit sapiente suscipit autem iste magnam atque libero. Ipsa
+          harum itaque maiores. Debitis corrupti repudiandae nesciunt illo
+          perspiciatis. Asperiores minima quis, est ipsum assumenda eum animi
+          facere, sit non quae illum blanditiis labore dolorem ducimus! Rerum,
+          accusantium quos corrupti dolores sed possimus obcaecati et iure
+          consequuntur doloremque aspernatur perspiciatis non voluptate aliquid
+          ad neque fugiat modi? Repellat atque consectetur numquam fugiat
+          repellendus accusantium, cupiditate dolorem aperiam consequuntur
+          quisquam aliquam sit explicabo ipsum eum obcaecati nisi cumque? Sequi
+          consequatur aut enim aliquam quis blanditiis vitae rem Lorem ipsum
+          dolor sit, amet consectetur adipisicing elit. Consequuntur, possimus
+          tenetur assumenda et veniam eligendi adipisci ad. Magni consequuntur
+          odio voluptates, cupiditate unde fuga aperiam nobis. Dicta veritatis
+          excepturi officiis aut, harum, odit sapiente suscipit autem iste
+          magnam atque libero. Ipsa
+        </div>
+      </>
     </section>
   );
 };
