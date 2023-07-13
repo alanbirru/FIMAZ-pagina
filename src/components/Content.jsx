b9aca8f21ca646459687f347ddacc8b9732195fe
alanbirru <alanb_99hotmail.com>
2023-07-13 09:41:57 -0700

    adding padding on the content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,24 +6,30 @@ const Content = ({ navItems }) => {
   const paddingTop = "pt-28";
 
   return (
-    <div className=" mx-auto flex w-[90%] flex-col gap-10    text-gray-700 md:w-[100%] md:gap-40 md:text-xl">
-      <div className=" bg-gray-200 pb-28">
+    <div className=" mx-auto flex w-[100%] flex-col gap-10  text-gray-700 md:w-[100%] md:gap-40 md:text-xl ">
+      <div className=" bg-gray-200  p-7 pb-40">
         <QuienesSomos
           title={navItems[1].SectionTitle}
           paddingTop={paddingTop}
           id={navItems[1].id}
         />
       </div>
-      <Precios
-        title={navItems[2].SectionTitle}
-        paddingTop={paddingTop}
-        id={navItems[2].id}
-      />
-      <Contacto
-        title={navItems[3].SectionTitle}
-        paddingTop={paddingTop}
-        id={navItems[3].id}
-      />
+
+      <div className=" p-7">
+        <Precios
+          title={navItems[2].SectionTitle}
+          paddingTop={paddingTop}
+          id={navItems[2].id}
+        />
+      </div>
+
+      <div className="p-7">
+        <Contacto
+          title={navItems[3].SectionTitle}
+          paddingTop={paddingTop}
+          id={navItems[3].id}
+        />
+      </div>
     </div>
   );
 };
