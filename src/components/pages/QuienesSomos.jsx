6cd985ed9bee5ffbb77414e72735f90dfafeea73
alanbirru <alanb_99hotmail.com>
2023-07-13 11:02:56 -0700

    changing dicrea logo

--- src/components/pages/QuienesSomos.jsx
+++ src/components/pages/QuienesSomos.jsx
@@ -8,7 +8,7 @@ const QuienesSomos = ({ paddingTop, title, id }) => {
         <div className="  flex flex-wrap items-center justify-center gap-14 ">
           <img className=" w-36 md:w-[350px]" src={DicreaLogo} />
 
-          <p className="font-weight: 900; w-[800px] text-center text-white">
+          <p className="font-weight: 900; w-[800px] text-center text-white  ">
             En DICREA, nos enorgullece ser una empresa dedicada al diseño y
             marketing digital, con una especialización destacada en el sector
             inmobiliario. Nuestra pasión por la creatividad y la tecnología nos
