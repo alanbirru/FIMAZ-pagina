de3a1044b3a6bcd13c37f5405f685af6495216bd
alanbirru <alanb_99hotmail.com>
2023-07-14 10:55:25 -0700

    estructura del evento images

--- src/components/UpBtn.jsx
+++ src/components/UpBtn.jsx
@@ -41,7 +41,7 @@ const UpBtn = () => {
   return (
     <button
       onClick={handleClick}
-      className={`fixed bottom-6 right-6  rounded-xl  bg-gradient-to-r from-orange-500 to-orange-400 p-3 text-white  drop-shadow-2xl transition-all duration-300 ${buttonVisibilityClass} ${scalingClass}`}
+      className={`fixed bottom-6 right-6  rounded-xl  bg-gradient-to-r from-orange-500 to-orange-400 p-3 text-white transition-all duration-300 ${buttonVisibilityClass} ${scalingClass}`}
     >
       <TiArrowUpThick className="text-lg md:text-2xl" />
     </button>
--- src/components/pages/Precios.jsx
+++ src/components/pages/Precios.jsx
@@ -1,3 +1,7 @@
+import estructuraUno from "../../assets/Estructura del evento/estructura-1.jpg";
+import estructuraDos from "../../assets/Estructura del evento/estructura-2.jpg";
+import estructuraTres from "../../assets/Estructura del evento/estructura-3.jpg";
+
 const Precios = ({ paddingTop, title, id }) => {
   return (
     <section className={`${paddingTop} `} id={id}>
@@ -5,27 +9,62 @@ const Precios = ({ paddingTop, title, id }) => {
         {title}
         <div className=" flex flex-col justify-center">
           <div className="flex flex-col justify-center">
-            <h2 className="text-center">
+            <h3 className="mb-10 text-center">
               ¿<span className=" text-primary">Dónde</span> se llevara acabo?
-            </h2>
+            </h3>
           </div>
 
           <div className="flex flex-col justify-center">
-            <h2 className="text-center">
-              ¿Cuando <span className="text-primary">será</span>?
-            </h2>
+            <h3 className=" mb-10 text-center ">
+              ¿Cuando <span className="mb text-primary">será</span>?
+            </h3>
           </div>
 
           <div className="flex flex-col justify-center">
-            <h2 className="text-center">
+            <h3 className="mb-10 text-center ">
               ¿Como será <span className="text-primary">acceso</span>?
-            </h2>
+            </h3>
           </div>
 
           <div className="flex flex-col justify-center">
-            <h2 className="text-center">
+            <h3 className="mb-10 text-center ">
               ¿Quienes podrán <span className="text-primary">asistir</span>?
-            </h2>
+            </h3>
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <h3 className="mb-10 text-center ">
+              Estructura del <span className="text-primary">evento</span>
+            </h3>
+            <div className="flex flex-col items-center justify-center gap-10 ">
+              <a href={estructuraUno}>
+                <img
+                  className=" w-[500px] drop-shadow-xl"
+                  src={estructuraUno}
+                  alt="image"
+                />
+              </a>
+
+              <a href={estructuraDos}>
+                <img
+                  className="w-[500px] drop-shadow-xl"
+                  src={estructuraDos}
+                  alt="image"
+                />
+              </a>
+
+              <a href={estructuraTres}>
+                <img
+                  className=" w-[1000px] drop-shadow-xl"
+                  src={estructuraTres}
+                  alt="image"
+                />
+              </a>
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <h3 className="mb-10 text-center ">Stands</h3>
           </div>
         </div>
       </>
