81da511e3a4c5580aa3e57f629bfad1966f108b4
alanbirru <alanb_99hotmail.com>
2023-07-17 16:30:54 -0700

    centering hamburger icon

--- src/components/Navbar/Hamburger.jsx
+++ src/components/Navbar/Hamburger.jsx
@@ -5,13 +5,13 @@ const Hamburger = ({ IsOpen, onClick }) => {
   return (
     <span onClick={onClick}>
       <FiMenu
-        className={`@apply duration-[1s]; absolute right-4 top-5 cursor-pointer text-2xl text-gray-700 transition-opacity md:hidden  ${
+        className={`@apply duration-[1s]; absolute right-4 top-6 cursor-pointer text-2xl text-gray-700 transition-opacity md:hidden  ${
           !IsOpen ? " opacity-100" : "opacity-0"
         } transition ease-out `}
       />
 
       <IoIosClose
-        className={`@apply duration-[1s]; absolute right-1 top-2 cursor-pointer text-5xl text-gray-700 transition-opacity md:hidden ${
+        className={`@apply duration-[1s]; absolute right-1 top-3 cursor-pointer text-5xl text-gray-700 transition-opacity md:hidden ${
           IsOpen ? " text-red-500 opacity-100" : "opacity-0"
         }`}
       />
