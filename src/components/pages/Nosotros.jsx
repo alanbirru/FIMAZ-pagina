61d8fea2a29051997ef3232645ccf631178bc127
alanbirru <alanb_99hotmail.com>
2023-07-17 12:53:44 -0700

    changing flex directions

--- src/components/pages/Nosotros.jsx
+++ src/components/pages/Nosotros.jsx
@@ -5,15 +5,15 @@ const Nosotros = ({ paddingTop, title, id }) => {
     <div>
       <section className={`mt-[20px]  ${paddingTop}`} id={id}>
         {title}
-        <div className="  flex flex-wrap items-center justify-center gap-14 ">
-          <img className=" w-36 md:w-[350px]" src={DicreaLogo} />
-          <p className="font-weight: 900; w-[800px] text-center text-white  ">
-            FIMAZ es una feria especializada en el sector inmobiliario que
-            permite a profesionales y empresas exhibir proyectos, establecer
-            contactos y mantenerse actualizados con las últimas tendencias del
-            mercado. Los interesados en bienes raíces encuentran información
-            relevante y conexiones valiosas.
+        <div className="  flex flex-row-reverse flex-wrap items-center justify-center gap-14 md:flex-row md:gap-24 ">
+          <p className=" w-[550px] text-center text-white">
+            DICREA es una empresa especializada en diseño y marketing digital,
+            con un enfoque destacado en el sector inmobiliario. Su pasión por la
+            creatividad y la tecnología los motiva a ofrecer soluciones
+            innovadoras y efectivas para ayudar a sus clientes a sobresalir en
+            un mercado altamente competitivo.
           </p>
+          <img className=" w-40 md:w-[350px]" src={DicreaLogo} />
         </div>
 
         {/* <TypeAnimation
